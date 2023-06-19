32a31926c38b78580347c159b416154d58aefee8
Adam Kowalski <adam.kowalski.work@gmail.com>
2023-06-19 00:08:40 -0700

    incrase and decrease score

--- src/routes/games/[slug]/hole/[hole_number]/+page.server.ts
+++ src/routes/games/[slug]/hole/[hole_number]/+page.server.ts
@@ -1,5 +1,7 @@
-import type { PageServerLoad } from './$types';
+import { fail } from '@sveltejs/kit';
+import type { Actions, PageServerLoad } from './$types';
 import { createClient } from '@vercel/postgres';
+import { z } from 'zod';
 
 export const load: PageServerLoad = async ({ params }) => {
 	const client = createClient();
@@ -30,4 +32,63 @@ export const load: PageServerLoad = async ({ params }) => {
 		hole,
 		scores
 	};
+};
+
+export const actions: Actions = {
+	increase_score: async ({ params, request }) => {
+		const data = Object.fromEntries(await request.formData());
+		const schema = z.object({ id: z.string().min(1) });
+		const parsed = schema.safeParse(data);
+		if (!parsed.success) return fail(400, { ...data, incorrect: true });
+		const client = createClient();
+		await client.connect();
+		await client.sql`BEGIN;`;
+		const game_result = await client.sql`
+			SELECT id, course_id FROM game WHERE slug=${params.slug};
+		`;
+		const game = game_result.rows[0];
+		await client.sql`
+			UPDATE player_scores
+			SET score=score+1
+			WHERE player_id=${parsed.data.id}
+			AND game_id=${game.id}
+			AND hole_id=(
+				SELECT id
+				FROM hole
+				WHERE hole_number=${params.hole_number}
+				AND course_id=${game.course_id}
+			);
+		`;
+
+		await client.sql`COMMIT;`;
+		await client.end();
+	},
+	decrease_score: async ({ params, request }) => {
+		const data = Object.fromEntries(await request.formData());
+		const schema = z.object({ id: z.string().min(1) });
+		const parsed = schema.safeParse(data);
+		if (!parsed.success) return fail(400, { ...data, incorrect: true });
+		const client = createClient();
+		await client.connect();
+		await client.sql`BEGIN;`;
+		const game_result = await client.sql`
+			SELECT id, course_id FROM game WHERE slug=${params.slug};
+		`;
+		const game = game_result.rows[0];
+		await client.sql`
+			UPDATE player_scores
+			SET score=GREATEST(0, score-1)
+			WHERE player_id=${parsed.data.id}
+			AND game_id=${game.id}
+			AND hole_id=(
+				SELECT id
+				FROM hole
+				WHERE hole_number=${params.hole_number}
+				AND course_id=${game.course_id}
+			);
+		`;
+
+		await client.sql`COMMIT;`;
+		await client.end();
+	}
 };
--- src/routes/games/[slug]/hole/[hole_number]/+page.svelte
+++ src/routes/games/[slug]/hole/[hole_number]/+page.svelte
@@ -4,7 +4,6 @@
 	import type { PageData } from './$types';
 
 	export let data: PageData;
-	console.log(data);
 </script>
 
 <header class="pt-2">
@@ -13,21 +12,42 @@
 
 <main class="flex flex-col p-2 gap-2">
 	<hr />
-	<section>
-		<strong>Par</strong>
-		<p>{data.hole.par}</p>
-	</section>
-	<hr />
 	<strong>Scores</strong>
 	{#each data.scores as score}
 		<form method="POST" use:enhance class="flex items-center gap-2 border p-2 rounded shadow">
 			<input type="hidden" name="id" value={score.id} />
 			<p class="flex-1">{score.name}</p>
-			<button class="rounded shadow bg-blue-500 w-8 h-8 hover:bg-blue-700 text-white">-</button>
+			<button
+				formaction="?/decrease_score"
+				class="rounded shadow bg-blue-500 w-8 h-8 hover:bg-blue-700 text-white">-</button
+			>
 			<strong class="text-xl">{score.score}</strong>
-			<button class="rounded shadow bg-blue-500 w-8 h-8 hover:bg-blue-700 text-white">+</button>
+			<button
+				formaction="?/increase_score"
+				class="rounded shadow bg-blue-500 w-8 h-8 hover:bg-blue-700 text-white">+</button
+			>
 		</form>
 	{/each}
+	<hr />
+	<section class="flex gap-2">
+		<a
+			href="/games/{data.game.slug}/hole/{data.hole.hole_number - 1}"
+			class="bg-blue-500 hover:bg-blue-700 text-white p-2 flex-1 text-center"
+		>
+			Previous
+		</a>
+		<a
+			href="/games/{data.game.slug}/hole/{data.hole.hole_number + 1}"
+			class="bg-blue-500 hover:bg-blue-700 text-white p-2 flex-1 text-center"
+		>
+			Next
+		</a>
+	</section>
+	<hr />
+	<section>
+		<strong>Par</strong>
+		<p>{data.hole.par}</p>
+	</section>
 	<hr />
 	<section>
 		<strong>Distance To Basket</strong>
