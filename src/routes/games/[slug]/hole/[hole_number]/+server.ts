1b48ed4084e751f47e5c0b62b01e8b5c7005b2e6
Adam Kowalski <adam.kowalski.work@gmail.com>
2023-06-25 22:08:23 -0700

    optimize query

--- src/routes/games/[slug]/hole/[hole_number]/+server.ts
+++ src/routes/games/[slug]/hole/[hole_number]/+server.ts
@@ -21,18 +21,20 @@ export const POST: RequestHandler = async ({ request, params }) => {
 		SELECT id, course_id FROM game WHERE slug=${params.slug};
 	`;
 	const game = game_result.rows[0];
+	const hole_result = await client.sql`
+		SELECT id
+		FROM hole
+		WHERE hole_number=${params.hole_number}
+		AND course_id=${game.course_id}
+	`;
+	const hole = hole_result.rows[0];
 	for (const player of parsed.data) {
 		await client.sql`
 			UPDATE player_scores
 			SET score=${player.score}
 			WHERE player_id=${player.id}
 			AND game_id=${game.id}
-			AND hole_id=(
-				SELECT id
-				FROM hole
-				WHERE hole_number=${params.hole_number}
-				AND course_id=${game.course_id}
-			);
+			AND hole_id=${hole.id};
 		`;
 	}
 	await client.sql`COMMIT;`;
