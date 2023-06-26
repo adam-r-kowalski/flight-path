dd1077c4b0751462cc4751ecdd2222681e33e2c0
Adam Kowalski <adam.kowalski.work@gmail.com>
2023-06-25 22:03:47 -0700

    batch updates sent to serer

--- src/routes/games/[slug]/hole/[hole_number]/+page.svelte
+++ src/routes/games/[slug]/hole/[hole_number]/+page.svelte
@@ -3,6 +3,28 @@
 	import type { PageData } from './$types';
 
 	export let data: PageData;
+
+	let pendingActions = 0;
+
+	const updateScores = async () => {
+		await fetch(`/games/${data.game.slug}/hole/${data.hole.hole_number}`, {
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/json'
+			},
+			body: JSON.stringify(data.scores)
+		});
+		pendingActions = 0;
+	};
+
+	$: if (pendingActions > 0) {
+		const current = pendingActions;
+		setTimeout(async () => {
+			if (pendingActions === current) {
+				await updateScores();
+			}
+		}, 2000);
+	}
 </script>
 
 <header class="pt-2 flex justify-center">
@@ -14,17 +36,33 @@
 <main class="flex flex-col p-2 gap-2">
 	<hr />
 	<strong>Scores</strong>
-	{#each data.scores as score}
+	{#each data.scores as score, i}
 		<section class="flex items-center gap-2 border p-2 rounded shadow">
 			<p class="flex-1">{score.name}</p>
 			<form method="POST" action="?/decrease_score" use:enhance>
 				<input type="hidden" name="id" value={score.id} />
-				<button class="rounded shadow bg-blue-500 w-8 h-8 hover:bg-blue-700 text-white">-</button>
+				<button
+					class="rounded shadow bg-blue-500 w-8 h-8 hover:bg-blue-700 text-white"
+					on:click|preventDefault={() => {
+						pendingActions += 1;
+						data.scores[i].score = Math.max(data.scores[i].score - 1, 0);
+					}}
+				>
+					-
+				</button>
 			</form>
 			<strong class="text-xl w-6 text-center">{score.score}</strong>
 			<form method="POST" action="?/increase_score" use:enhance>
 				<input type="hidden" name="id" value={score.id} />
-				<button class="rounded shadow bg-blue-500 w-8 h-8 hover:bg-blue-700 text-white">+</button>
+				<button
+					class="rounded shadow bg-blue-500 w-8 h-8 hover:bg-blue-700 text-white"
+					on:click|preventDefault={() => {
+						pendingActions += 1;
+						data.scores[i].score += 1;
+					}}
+				>
+					+
+				</button>
 			</form>
 		</section>
 	{/each}
@@ -33,12 +71,22 @@
 		<a
 			href="/games/{data.game.slug}/hole/{data.hole.hole_number - 1}"
 			class="bg-blue-500 hover:bg-blue-700 text-white p-2 flex-1 text-center"
+			on:click={async () => {
+				if (pendingActions > 0) {
+					await updateScores();
+				}
+			}}
 		>
 			Previous
 		</a>
 		<a
 			href="/games/{data.game.slug}/hole/{data.hole.hole_number + 1}"
 			class="bg-blue-500 hover:bg-blue-700 text-white p-2 flex-1 text-center"
+			on:click={async () => {
+				if (pendingActions > 0) {
+					await updateScores();
+				}
+			}}
 		>
 			Next
 		</a>
---
+++ src/routes/games/[slug]/hole/[hole_number]/+server.ts
@@ -0,0 +1,41 @@
+import { error, json } from '@sveltejs/kit';
+import type { RequestHandler } from './$types';
+import { z } from 'zod';
+import { createClient } from '@vercel/postgres';
+
+export const POST: RequestHandler = async ({ request, params }) => {
+	const data = await request.json();
+	const schema = z.array(
+		z.object({
+			id: z.number().positive(),
+			name: z.string().min(1),
+			score: z.number().positive()
+		})
+	);
+	const parsed = schema.safeParse(data);
+	if (!parsed.success) throw error(400, parsed.error.message);
+	const client = createClient();
+	await client.connect();
+	await client.sql`BEGIN;`;
+	const game_result = await client.sql`
+		SELECT id, course_id FROM game WHERE slug=${params.slug};
+	`;
+	const game = game_result.rows[0];
+	for (const player of parsed.data) {
+		await client.sql`
+			UPDATE player_scores
+			SET score=${player.score}
+			WHERE player_id=${player.id}
+			AND game_id=${game.id}
+			AND hole_id=(
+				SELECT id
+				FROM hole
+				WHERE hole_number=${params.hole_number}
+				AND course_id=${game.course_id}
+			);
+		`;
+	}
+	await client.sql`COMMIT;`;
+	await client.end();
+	return json({ message: 'Scores updated successfully' });
+};
